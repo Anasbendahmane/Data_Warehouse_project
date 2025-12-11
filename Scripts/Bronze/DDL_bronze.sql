56b6a3c007f7a20ce402f0170898a2a0cf7ca0c8
Anas <155098093+Anasbendahmane@users.noreply.github.com>
2025-12-11 18:21:00 +0100

    Create tables for CRM and ERP data in DDL_bronze.sql
    Added multiple tables for customer, product, sales details, and ERP information.

--- Scripts/Bronze/DDL_bronze.sql
+++ Scripts/Bronze/DDL_bronze.sql
@@ -1,5 +1,4 @@
 
-
 create table bronze.crm_cust_info (
 	cst_id int not null,
 	cst_key nvarchar(50),
@@ -9,3 +8,51 @@ create table bronze.crm_cust_info (
 	cst_gndr nvarchar(50),
 	cst_create_date Date
 );
+
+GO
+create table bronze.crm_prd_info(
+	prd_id int not null,
+	prd_key nvarchar(50),
+	prd_nm nvarchar(50),
+	prd_cost int,
+	prd_line nvarchar(50),
+	prd_start_dt datetime,
+	prd_end_dt datetime
+
+);
+GO
+create table bronze.crm_sales_details(
+	sls_ord_num nvarchar(50),
+	sls_prd_key nvarchar(50),
+	sls_cust_id int,
+	sls_order_dt int,
+	sls_ship_dt int,
+	sls_due_dt int,
+	sls_sales int,
+	sls_quantity int,
+	sls_price int
+
+);
+GO
+
+create table bronze.erp_cust_az12(
+	cid nvarchar(50),
+	bdate date,
+	gen nvarchar(50)
+
+);
+GO
+
+create table bronze.erp_loc_a101(
+	cid nvarchar(50),
+	cntry nvarchar(50)
+);
+GO
+
+create table bronze.erp_px_cat_g1v2 (
+	id nvarchar(50),
+	cat nvarchar(50),
+	subcat nvarchar(50),
+	maintenance nvarchar(50)
+);
+GO
