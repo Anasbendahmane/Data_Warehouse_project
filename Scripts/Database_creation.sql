60100978cda43946f45d44ba8b81c1a58b8e6f76
Anas <155098093+Anasbendahmane@users.noreply.github.com>
2025-12-11 18:46:26 +0100

    Implement drop and recreate for datawarehousing database
    Added logic to drop and recreate the datawarehousing database if it exists.

--- Scripts/Database_creation.sql
+++ Scripts/Database_creation.sql
@@ -1,5 +1,16 @@
 use master;
 GO
+
+
+-- Drop and recreate the database
+IF EXISTS (SELECT 1 from sys.databases Where name ='datawarehousing')
+BEGIN 
+  ALTER DATABASE datawarehousing SET SINGLE_USER with ROLLBACK IMMEDIATE ;-- Locks the datawarehousing database for exclusive admin access, immediately forcing the disconnection (ROLLBACK IMMEDIATE) of all other users to prevent blocking the maintenance operation.
+  DROP DATABASE datawarehousing.
+END;
+GO
+
+  
 --- Create the database fot the project datawarehousing
 Create database Datawarehousing;
 GO
