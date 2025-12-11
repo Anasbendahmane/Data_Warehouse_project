bf3c8f0b8e23207692c121e88ebe3bf9e8d58c42
Anas <155098093+Anasbendahmane@users.noreply.github.com>
2025-12-11 02:24:10 +0100

    Create DDL for crm_cust_info table in Bronze schema

---
+++ Scripts/Bronze/DDL_bronze.sql
@@ -0,0 +1,11 @@
+
+
+create table bronze.crm_cust_info (
+	cst_id int not null,
+	cst_key nvarchar(50),
+	cst_firstname nvarchar(50),
+	cst_lastname nvarchar(50),
+	cst_marital_status nvarchar(50),
+	cst_gndr nvarchar(50),
+	cst_create_date Date
+);
