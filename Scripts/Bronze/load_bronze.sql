fe8106d7c93b8bc4be6cbec56c6a12c1f53f3264
Anas <155098093+Anasbendahmane@users.noreply.github.com>
2025-12-15 01:17:38 +0100

    Create stored procedure for loading bronze data
    Refactored SQL script to create a stored procedure for loading data into the bronze layer. Added error handling and organized the loading process for CRM and ERP tables.

--- Scripts/Bronze/load_bronze.sql
+++ Scripts/Bronze/load_bronze.sql
@@ -1,80 +1,90 @@
-Use Datawarehousing;
-GO
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+
+BEGIN
+	BEGIN TRY
+		PRINT 'Loading Data into the bronze layer';
+		PRINT '--------------------------------------';
+		PRINT 'Loading CRM Tables';
+		PRINT '--------------------------------------';
+		TRUNCATE TABLE bronze.crm_cust_info
+		BULK INSERT bronze.crm_cust_info
+		FROM 'C:\Users\MSI\Dropbox\Mon PC (DESKTOP-LC36MF4)\Desktop\Data-warehouse-project\datasets\source_crm\cust_info.csv'
+		WITH (
+			FIRSTROW = 2, -- Instructs the loader to skip the first row (the header) of the CSV file
+			FIELDTERMINATOR =',', -- how the data is splited
+			TABLOCK ---- Acquires an exclusive lock on the entire table for the duration of the bulk load operation to ensure data integrity and faster loading
+		);
 
 
-TRUNCATE TABLE bronze.crm_cust_info
-GO
+		-------------------------------------------------
+		TRUNCATE TABLE bronze.crm_prd_info
 
-BULK INSERT bronze.crm_cust_info
-FROM 'C:\Users\MSI\Dropbox\Mon PC (DESKTOP-LC36MF4)\Desktop\Data-warehouse-project\datasets\source_crm\cust_info.csv'
-WITH (
-	FIRSTROW = 2, -- Instructs the loader to skip the first row (the header) of the CSV file
-	FIELDTERMINATOR =',', -- how the data is splited
-	TABLOCK ---- Acquires an exclusive lock on the entire table for the duration of the bulk load operation to ensure data integrity and faster loading
-);
-GO
 
--------------------------------------------------
-TRUNCATE TABLE bronze.crm_prd_info
-GO
+		BULK INSERT bronze.crm_prd_info
+		FROM 'C:\Users\MSI\Dropbox\Mon PC (DESKTOP-LC36MF4)\Desktop\Data-warehouse-project\datasets\source_crm\prd_info.csv'
+		WITH(
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-BULK INSERT bronze.crm_prd_info
-FROM 'C:\Users\MSI\Dropbox\Mon PC (DESKTOP-LC36MF4)\Desktop\Data-warehouse-project\datasets\source_crm\prd_info.csv'
-WITH(
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
 
-------------------------------------------------------------
-TRUNCATE TABLE bronze.crm_sales_details
-GO
+		------------------------------------------------------------
+		TRUNCATE TABLE bronze.crm_sales_details
 
-BULK INSERT bronze.crm_sales_details
-FROM 'C:\Users\MSI\Dropbox\Mon PC (DESKTOP-LC36MF4)\Desktop\Data-warehouse-project\datasets\source_crm\sales_details.csv'
-WITH(
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
 
-------------------------------------------------------------
-TRUNCATE TABLE bronze.erp_cust_az12
-GO
+		BULK INSERT bronze.crm_sales_details
+		FROM 'C:\Users\MSI\Dropbox\Mon PC (DESKTOP-LC36MF4)\Desktop\Data-warehouse-project\datasets\source_crm\sales_details.csv'
+		WITH(
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-BULK INSERT bronze.erp_cust_az12
-FROM 'C:\Users\MSI\Dropbox\Mon PC (DESKTOP-LC36MF4)\Desktop\Data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
---------------------------------------------------------
+		PRINT '--------------------------------------------';
+		PRINT 'Loading ERP Tables';
+		PRINT '--------------------------------------------';
+		TRUNCATE TABLE bronze.erp_cust_az12
 
-TRUNCATE TABLE bronze.erp_loc_a101
-GO
 
-BULK INSERT bronze.erp_loc_a101
-FROM 'C:\Users\MSI\Dropbox\Mon PC (DESKTOP-LC36MF4)\Desktop\Data-warehouse-project\datasets\source_erp\LOC_A101.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
+		BULK INSERT bronze.erp_cust_az12
+		FROM 'C:\Users\MSI\Dropbox\Mon PC (DESKTOP-LC36MF4)\Desktop\Data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
 
-------------------------------------------------------------------
-TRUNCATE TABLE bronze.erp_px_cat_g1v2
-GO
+		--------------------------------------------------------
 
-BULK INSERT bronze.erp_px_cat_g1v2
-FROM 'C:\Users\MSI\Dropbox\Mon PC (DESKTOP-LC36MF4)\Desktop\Data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
-WITH (
-	FIRSTROW = 2,
-	FIELDTERMINATOR = ',',
-	TABLOCK
-);
-GO
+		TRUNCATE TABLE bronze.erp_loc_a101
+
+		BULK INSERT bronze.erp_loc_a101
+		FROM 'C:\Users\MSI\Dropbox\Mon PC (DESKTOP-LC36MF4)\Desktop\Data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+
+		------------------------------------------------------------------
+		TRUNCATE TABLE bronze.erp_px_cat_g1v2
+
+
+		BULK INSERT bronze.erp_px_cat_g1v2
+		FROM 'C:\Users\MSI\Dropbox\Mon PC (DESKTOP-LC36MF4)\Desktop\Data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		WITH (
+			FIRSTROW = 2,
+			FIELDTERMINATOR = ',',
+			TABLOCK
+		);
+	END TRY
+	BEGIN CATCH
+		PRINT '-------------------------------';
+		PRINT 'An error occured';
+		PRINT 'ERROR MESSAGE: '+ ERROR_MESSAGE();
+		PRINT 'ERROR NUMBER: ' + CAST(ERROR_NUMBER() AS NVARCHAR(50));
+		PRINT 'ERROR LINE: ' + CAST(ERROR_LINE() as NVARCHAR(50));
+		PRINT 'ERROR PROCEDURE: ' + ERROR_PROCEDURE();
+	END CATCH
+END
